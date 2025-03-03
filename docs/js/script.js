550a43f0d14e6003b4e89bdb36e68c2e811237f2
exe-zip <harisamaruki1009@gmail.com>
2025-03-03 15:52:43 +0900

    デザイン大幅改変テスト

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,3 +3,13 @@ function PicAkane(){
 function PicRei(){
   document.getElementById("show-img").src="images/chara/rei.png";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
+
+  menuToggle.addEventListener("click", () => {
+      const isOpen = menu.style.left === "0px";
+      menu.style.left = isOpen ? "-300px" : "0px";
+  });
+});
